ed49ab447d230be8401483a1288ef531375eb44a
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-12 01:26:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,4 +25,12 @@ class Song
     song.save
     song 
   end 
+  
+  def find_by_name(name)
+    Song.all.detect { |song| song.name == name }
+  end 
+  
+  def find_or_create_by_name(name)
+    
+  end 
 end 
