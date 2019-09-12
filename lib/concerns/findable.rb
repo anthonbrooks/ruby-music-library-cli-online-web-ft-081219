54f7a5be2cb0b3565eb6457e9fb41e3133dcfd16
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-12 02:16:26 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,9 @@
 module Concerns::Findable
-  #module ClassMethods
-  #end 
+  def find_by_name(name)
+    self.all.detect { |song| song.name == name }
+  end 
   
-  #module InstanceMethods
-  #end 
+  def find_or_create_by_name(name)
+    self.find_by_name(name) || create(name)
+  end 
 end 
