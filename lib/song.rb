10c396baf0ca4b512a423467adfb81ee11b4799d
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-12 01:52:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,13 @@
 class Song 
   
-  attr_accessor :name, :artist
+  attr_accessor :name, :artist, :genre
   
   @@all = [] 
   
-  def initialize(name, artist=nil)
+  def initialize(name, artist=nil, genre=nil)
     @name = name 
     self.artist = artist if artist
+    self.genre = genre if genre
   end 
   
   def self.all
